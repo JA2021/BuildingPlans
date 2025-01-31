7cd2461f50d21a1363337d5b8bcdbe893d1c5d5a
JA2021 <zubera867@gmail.com>
2025-01-31 11:33:56 +0200

    Enhance comment functionality and UI in home component; add truncation logic and new buttons-Jehan 31-01-25

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -8218,6 +8218,10 @@ this.subscriptions.push(subscription);
           //   const originator: string = await this.getOriginatorName(current.createdById);
           //   tempApplication.Originator = originator;
           // }
+          // if (current.createdById != null) {
+          //   const originator: string = await this.getOriginatorName(current.createdById);
+          //   tempApplication.Originator = originator;
+          // }
 
 
           const address = current.physicalAddress
